b6f6a8f8cc5daff8331773becefc62f15c8817d9
原貴悌 <taka_raison@genkiteinoiMac.local>
2021-03-28 18:29:53 +0900

    DataStore model is created to fetch data

--- screens/HomeScreen/HomeScreen.tsx
+++ screens/HomeScreen/HomeScreen.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, {useEffect, useState} from 'react';
 import { StyleSheet, Image, FlatList} from 'react-native';
 
 import EditScreenInfo from '../../components/EditScreenInfo';
@@ -7,16 +7,33 @@ import styles from './styles'
 import categories from '../../assets/data/categories'
 import HomeCategory from '../../components/HomeCategory';
 
+// data
+import {DataStore} from 'aws-amplify'
+import {Category} from '../../models/index'
+
 //const firstCategory = categories.items[0]
 
 
 const HomeScreen=()=> {
+
+  const [categories, setCategories] = useState<Category[]>([]);
+
+useEffect(()=>{
+  const fetchCategories = async ()=>{
+   const response = await DataStore.query(Category)
+   console.log(response);
+   // setCategories(await DataStore.query(Category))
+   setCategories(response)
+};
+fetchCategories()
+},[])
+
   return (
     <View style={styles.container}>
       {/* list of categories */}
       {/* <HomeCategory category={firstCategory}/> */}
        <FlatList
-       data={categories.items}
+       data={categories}
        renderItem ={({item})=>(
         <HomeCategory category={item}/> 
        )}
